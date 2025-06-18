6a2115c62214ba3d639a3625dbb7e0e3285bb05f
cipher <ciphershadow1@gmail.com>
2025-06-18 04:14:05 +0000

    print the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - Print the alphabet in lowercase letters
+  *
+  * Return: Always 0
+  */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n')
+
+	return (0);
+}
