2e5d05d323455d393c00f8d00cebca088db00e09
cipher <ciphershadow1@gmail.com>
2025-06-18 03:54:38 +0000

    new main and program file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
 
 /**
  * main - Entry point
- *
+ * Description: It prints the word _putchar, followed by a new line.
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	char word(8) = "_putchar";
+	int i;
+
+	for(i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	_putchar('\n');
+	}
+
 	return (0);
 }
