8c65e33d1ca034fb5338c7d0d645a7bf853424c0
cipher <ciphershadow1@gmail.com>
2025-06-20 04:17:53 +0000

    betty strikes again with the description

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
-  * Add function - refer to Description
-  * Description: This add function adds two integers and returns result.
-  * Return: (0) success
+  * add - refer to Description
+  * @a: An integer a
+  * @b: An integer b
+  * Description: Function that adds two integers and returns the result.
+  * Return: returns the result of a plus b
   */
 
 int add(int a, int b)
