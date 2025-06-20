238c60d042d9d2c6744dd96fe011f5cc34e07e73
cipher <ciphershadow1@gmail.com>
2025-06-20 04:14:38 +0000

    updated description

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-  * -add - refer to Description
-  * Description: Adds two integers and returns result.
+  * Add function - refer to Description
+  * Description: This add function adds two integers and returns result.
   * Return: (0) success
   */
 
