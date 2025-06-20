7281cdb14120aa11dcb170bee13a6e5ebc69a963
cipher <ciphershadow1@gmail.com>
2025-06-20 04:13:00 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * -add - refer to Description
+  * Description: Adds two integers and returns result.
+  * Return: (0) success
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
