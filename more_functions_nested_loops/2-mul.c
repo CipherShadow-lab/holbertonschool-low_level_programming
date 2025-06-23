b1e964bc2558f544dcaed08b53499ea6f409ea0d
cipher <ciphershadow1@gmail.com>
2025-06-23 05:08:09 +0000

    Function to muliply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Description: mul - Multiplies two integers.
+ * @a: First integer to multiply
+ * @b: Second integer to multiply
+ * Return: 0 success
+ */
+
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+	return (0);
+}
