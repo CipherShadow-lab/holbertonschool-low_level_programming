74c10aaba4bcab6162bef205f0609ca6e3eacfd8
cipher <ciphershadow1@gmai.com>
2025-06-24 03:53:53 +0000

    copied from previous file for editing

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Description: main - Prints Fizz, Buzz and FizzBuzz
+ *
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	int c;
+	char f[5] = "Fizz";
+	char b[5] = "Buzz";
+
+	for (c = 1; c <= 100; c++)
+	{
+		if (c % 3 == 0 && c % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("%s", f);
+		}
+		else if (c % 5 == 0)
+		{
+			printf("%s", b);
+		}
+		else
+		{
+			printf("%d", c);
+		}
+		if (c != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
