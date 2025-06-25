0848c0e31503bd763bfb6a7d3193c1b963549add
cipher <ciphershadow1@gmail.com>
2025-06-25 06:17:25 +0000

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Refer to Description
+ * @str: String to be printed
+ * Description: Function prints a string followed by new line to stdout.
+ * Return: (0) success
+ */
+
+int _puts(char *str)
+{
+	char *str;
+
+	_put(str);
+
+	return (0);
+}
