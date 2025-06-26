82f7a07bb4674fd57a7d1f23ea6cfb59b53ba7bd
cipher <ciphershadow1@gmail.com>
2025-06-26 07:01:16 +0000

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_array - Refer to Description
+ * @a: Pointer to array
+ * @n: The array to be printed
+ * Description: Function prints n elements of an array of integers.
+ * Return: (0) success
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+	}
+	if (i != (n - 1))
+	{
+		printf(", ");
+	}
+	printf("\n");
+}
