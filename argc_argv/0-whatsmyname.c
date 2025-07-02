89c19af596c765f3c63699cef288502572d12a0e
cipher <ciphershadow1@gmail.com>
2025-07-02 07:50:36 +0000

    updated program file with edited comments

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* Main - Prints the program name followed by a new line.
+* @argc: number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments
+* Return: Always 0 (success)
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
