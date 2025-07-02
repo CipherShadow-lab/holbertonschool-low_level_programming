2d2adca1d9eb4ac17da610432b2e87b8731c4214
cipher <ciphershadow1@gmail.com>
2025-07-02 07:57:12 +0000

    betty is being difficult!

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 
 /**
-* Main - Prints the program name followed by a new line.
+* main - prints program name followed by a new line
+* Description: main prints the program name followed by a new line.
 * @argc: number of arguments supplied to the program.
 * @argv: An array of pointers to the arguments
 * Return: Always 0 (success)
 */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
