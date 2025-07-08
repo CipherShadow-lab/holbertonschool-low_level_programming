67f60f6ae7a23dedb01a0286caff65ada6033859
cipher <ciphershadow1@gmail.com>
2025-07-08 22:06:51 +0000

    added string concat file

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - Function concatenates two strings.
+ * @s1: First string to be copied
+ * @s2: Second string to be copied
+ * Return: Concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int length1 = 0;
+	int length2 = 0;
+	int index1 = 0;
+	int index2 = 0;
+	char *fullstr;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	while (s1[length1] != '\0')
+	{
+		length1++;
+	}
+	while (s2[length2] != '\0')
+	{
+		length2++;
+	}
+	fullstr = malloc(length1 + length2 + 1);
+	if (fullstr == NULL)
+	{
+		return (NULL);
+	}
+	while (index1 < length1)
+	{
+		fullstr[index1] = s1[index1];
+		index1++;
+	}
+	while (index2 <= length2)
+	{
+		fullstr[index1 + index2] = s2[index2];
+		index2++;
+	}
+	return (fullstr);
+}
