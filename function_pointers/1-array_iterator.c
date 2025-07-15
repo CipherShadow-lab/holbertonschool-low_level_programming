ab106ea0de6bec3914ada5344ce3141e722e990c
cipher <ciphershadow1@gmail.com>
2025-07-15 03:13:01 +0000

    added array iterator file

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - function executes another function
+ * to print each element of an array.
+ * @array: array to print
+ * @size: size of array
+ * @action: function that is called on to print the array
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
