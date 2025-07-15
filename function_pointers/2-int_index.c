d8e96b927e7a59dafc1dcb5548b53cbcb6cc733b
cipher <ciphershadow1@gmail.com>
2025-07-15 03:37:45 +0000

    added int index file

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+
+/**
+ * int_index - Function searches for an integer
+ * @array: array to search for integer
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to compare values
+ * Return: index of the first element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
