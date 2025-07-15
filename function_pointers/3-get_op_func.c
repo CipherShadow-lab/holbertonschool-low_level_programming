5e3212089bf1eacfcad62075bc9fe3d30c128857
cipher <ciphershadow1@gmail.com>
2025-07-15 04:36:49 +0000

    added all files for task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+  * get_op_func - Selects the correct function in order to
+  * perform the operation requested by the user
+  * @s: operator passed as an argument
+  * Return: A pointer to the function corresponding
+  * with the specified operator.
+  */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		return (ops[i].f);
+		i++;
+	}
+	return (0);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: Argument count
+ * @argv: Argument vector - array of strings.
+ * Return: 0 on success.
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
