28ef3f1047a5c67fab7791f313e8229774b5547a
cipher <ciphershadow1@gmail.com>
2025-07-30 23:09:46 +0000

    fixed wording and formatting in lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,17 +2,14 @@
 #define LISTS_H
 
 /**
- * lists - header file for singly and doubly linked lists
+ * struct list_s - singly linked list node
+ * @str: string dynamically allocated
+ * @len: length of string
+ * @next: pointer to the next node
+ *
+ * Description: This struct defines a node in a singly linked list.
  */
 
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
 typedef struct list_s
 {
 	char *str;
