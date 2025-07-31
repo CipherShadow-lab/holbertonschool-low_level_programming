e717cda8c834aa983d2acd3387df40adf1c9dd01
cipher <ciphershadow1@gmail.com>
2025-07-31 20:49:18 +1000

    added code for task 6 sum_dlistint

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - function returns the sum of all data (n) in a  dlistint_t list
+ * @head: pointer to head
+ *
+ * Return: sum of data (n)
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
