526bb92bc99605476f6ba4ad85267228d0905cdb
cipher <ciphershadow1@gmail.com>
2025-07-31 20:01:36 +0000

    fixed: spacing issue line 24

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (current != NULL && index < idx -1)
+	while (current != NULL && index < idx - 1)
 	{
 		current = current->next;
 		index++;
