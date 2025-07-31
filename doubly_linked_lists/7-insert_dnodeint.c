b0c05239f76c7cb535aad5017368ad4d50dd43cd
cipher <ciphershadow1@gmail.com>
2025-07-31 21:17:17 +1000

    added code for task 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function inserts a new node at a given position
+ * @h: double pointer to head
+ * @idx: index to insert
+ * @n: value to insert
+ *
+ * Return: NULL if failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode = NULL;
+	dlistint_t *current = *h;
+	unsigned int index = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (current != NULL && index < idx -1)
+	{
+		current = current->next;
+		index++;
+	}
+
+	if (current == NULL)
+		return (NULL);
+
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = current->next;
+	newNode->prev = current;
+
+	current->next->prev = newNode;
+	current->next = newNode;
+
+	return (newNode);
+}
