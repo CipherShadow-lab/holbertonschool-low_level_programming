1d2b842b98500e6110a7e5eea7171926025a0d12
cipher <ciphershadow1@gmail.com>
2025-08-01 16:06:50 +1000

    added 0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - function reads a text file and prints it to POSIX output
+ * @filename: name of file to read and print
+ * @letters: number of characters in file
+ *
+ * Return: number of letters read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read;
+	ssize_t bytes written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (0);
+}
