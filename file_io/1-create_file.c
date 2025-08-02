052560479391fff5ced8bded658499c613c02d51
cipher <ciphershadow1@gmail.com>
2025-08-02 03:05:19 +0000

    added function for 1-create_file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * create_file - function creates a file with content terminated with null
+ * @filename: name of file to read and print
+ * @text_content: null terminated string
+ *
+ * Return: 1 if successful and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written = 0;
+	size_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+		bytes_written = write(fd, text_content, len);
+
+		if (bytes_written == -1 || (size_t)bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
+}
