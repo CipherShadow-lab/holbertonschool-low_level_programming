f4afe50d36c0b317a5e7ca0ca661950a1ac7ff69
cipher <ciphershadow1@gmail.com>
2025-08-02 06:37:37 +0000

    fixed: read return issue

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -50,23 +50,23 @@ int main(int argc, char *argv[])
 
 	if (fd_to == -1)
 		handle_error("Error: Can't write to %s\n", argv[2], 99, fd_from);
-
-	r = read(fd_from, buf, 1024);
-	while (r > 0)
+	while (1)
 	{
+		r = read(fd_from, buf, 1024);
+		if (r == -1)
+			handle_error("Error: Can't read from file %s\n", argv[1], 98, fd_from);
+		if (r == 0)
+			break;
+
 		total = 0;
 		while (total < r)
 		{
 			w = write(fd_to, buf + total, r - total);
 			if (w == -1)
-				handle_error("Error: Can't write to %s\n", argv[2], 98, fd_from);
+				handle_error("Error: Can't write to %s\n", argv[2], 99, fd_to);
 			total += w;
 		}
-		r = read(fd_from, buf, 1024);
 	}
-	if (r == -1)
-		handle_error("Error: Can't read from file %s\n", argv[1], 98, fd_from);
-
 	if (close(fd_from) == -1)
 		handle_error("Error: Can't close fd %d\n", NULL, 100, fd_from);
 
