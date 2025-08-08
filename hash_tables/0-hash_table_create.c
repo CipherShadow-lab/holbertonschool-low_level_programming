2468fa6cc2ecd48fdbf2c541e54ff118493a0d00
CipherShadow <ciphershadow1@gmail.com>
2025-08-08 12:22:39 +1000

    added new dir and files for hash_tables project

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - finction creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to newly created hash table, else NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	//insert code here
+}
