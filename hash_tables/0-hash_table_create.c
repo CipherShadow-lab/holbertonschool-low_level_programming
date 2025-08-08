20c00852c95441d89b1ce4a40a7d7226ed6ef459
CipherShadow <ciphershadow1@gmail.com>
2025-08-08 12:53:13 +1000

    add task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,5 +9,29 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	//insert code here
+	unsigned long int i;
+	hash_table_t *table;
+
+	if (size == 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = o; i < size; i++)
+		table->array[i] = NULL;
+
+	table->size = size;
+	
+	return (table);
 }
