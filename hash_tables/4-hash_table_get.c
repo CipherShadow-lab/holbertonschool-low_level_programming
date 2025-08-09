5ddf2c365652428f0336e48942485d7562649f23
CipherShadow <ciphershadow1@gmail.com>
2025-08-09 11:38:23 +1000

    add task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+
+/**
+ * _strcmp - function compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 0 if equal
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
+/**
+ * hash_table_get - function gets value associated with a key
+ * @ht: pointer to hash table
+ * @key: key to search for
+ *
+ * Return: value associated with key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if(_strcmp(node->key, key) == 0)
+			return (node->value);
+
+		node = node->next;
+	}
+
+	return (NULL);
+}
